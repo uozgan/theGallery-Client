de9225cfe2a4250ba619330346c61dbe94f8c724
uozgan <utkuozgan_8@hotmail.com>
2020-04-01 14:27:00 +0200

    some bug fixes

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -26,14 +26,13 @@ export default function SignUp() {
   function submitForm(event) {
     event.preventDefault();
 
-    console.log("isArtist", isArtist);
+    //console.log("isArtist", isArtist);
 
     dispatch(signUp(name, email, password, isArtist));
 
     setEmail("");
     setPassword("");
     setName("");
-    setIsArtist(false);
   }
 
   return (
--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -28,14 +28,15 @@ const tokenStillValid = userWithoutToken => ({
 
 export const logOut = () => ({ type: LOG_OUT });
 
-export const signUp = (name, email, password) => {
+export const signUp = (name, email, password, isArtist) => {
   return async (dispatch, getState) => {
     dispatch(appLoading());
     try {
       const response = await axios.post(`${apiUrl}/signup`, {
         name,
         email,
-        password
+        password,
+        isArtist
       });
 
       dispatch(loginSuccess(response.data));
